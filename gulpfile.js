b469a3e18aef23ec8c17b42c45de34a0c866bb6e
Alex Yeahnotov <yeahhh.not@gmail.com>
2020-05-21 12:22:38 +0300

    Фикс генерации webp изображений.

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ gulp.task("images", function() {
 });
 
 gulp.task("webp", function() {
-  return gulp.src("source/img/**/*.{png,jpg}")
+  return gulp.src("source/img/*.{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("build/img"))
 });
