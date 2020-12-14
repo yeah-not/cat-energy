19c67fbb5f99490476af0b9cf11802071cae47cc
Yeah.not <yeahhh.not@gmail.com>
2020-12-14 17:28:50 +0200

    Добавил минимальную сборку на gulp: scss + остальное копированием, вотчеры + browserSync, задачи + скрипты в package.json. Взял доп настройки package.json из бэкапа (editorconfig, stylelint).

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const gulp = require('gulp');
+const {series, parallel} = require('gulp');
+const plumber = require('gulp-plumber');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+// Functions
+// ---------------
+function clean() {
+  return del('./build');
+}
+
+function html() {
+  return gulp.src('./source/*.html')
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.stream());
+}
+
+function styles() {
+  return gulp.src('./source/sass/style.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
+}
+
+function scripts() {
+  return gulp.src('source/js/*.js')
+    .pipe(gulp.dest('./build/js'))
+    .pipe(browserSync.stream());
+}
+
+function images() {
+  return gulp.src([
+      './source/img/*.{jpg,png,svg}',
+      './source/*.png'
+    ], {
+      base: './source'
+    })
+    .pipe(gulp.dest('./build'));
+}
+
+function sprite() {
+  return gulp.src('./source/img/svg-sprite/*.svg')
+    .pipe(gulp.dest('./build/img'));
+}
+
+function fonts() {
+  return gulp.src('./source/fonts/*.{woff,woff2}')
+    .pipe(gulp.dest('./build/fonts'));
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./build/"
+    }
+  });
+
+  gulp.watch('./source/*.html', html);
+  gulp.watch('./source/sass/**/*.scss', styles);
+  gulp.watch('./source/js/*.js', scripts);
+}
+
+
+// Tasks
+// ---------------
+gulp.task('build', series(clean,
+                   parallel(html, styles, scripts, images, sprite, fonts)));
+gulp.task('watch', series('build', watch));
